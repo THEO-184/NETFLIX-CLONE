2868defe6a56179c56be1d1ad092055c43666d7f
Theophilus Boakye Yiadom <theophilusboakye47@gmail.com>
2021-06-12 00:09:07 +0100

    added app files

---
+++ app.js
@@ -0,0 +1,33 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+	const btn = question.querySelector("span");
+	btn.addEventListener("click", (e) => {
+		questions.forEach((item) => {
+			if (item !== question) {
+				item.classList.remove("view");
+			}
+		});
+		question.classList.toggle("view");
+	});
+});
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function slideIn(e) {
+	sliderImages.forEach((sliderImage) => {
+		const scrollHeight =
+			window.pageYOffset + window.innerHeight - sliderImage.height / 1;
+		const imgBottom = sliderImage.offsetTop + sliderImage.height;
+		const isPastHalf = scrollHeight > sliderImage.offsetTop;
+		const isNotScrolledPast = window.pageYOffset < imgBottom;
+
+		if (isPastHalf && isNotScrolledPast) {
+			sliderImage.classList.add("active");
+		} else {
+			sliderImage.classList.remove("active");
+		}
+	});
+}
+
+window.addEventListener("scroll", slideIn);
